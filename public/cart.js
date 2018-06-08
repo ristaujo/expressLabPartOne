3ffb6c69237b441167477407bd04619af114e0b1
John <ristaujo@gmail.com>
2018-06-07 21:00:53 -0400

    More styling

--- public/cart.js
+++ public/cart.js
@@ -2,8 +2,14 @@
 
 const cart = {
   template: `
+  <h1>Grocery Time</h1>
+  <section class="input-main">
+    <input type="text" placeholder="Product">
+    <input type="text" placeholder="Price">
+    <input type="text" placeholder="Quantity">
+    <button class="add">Add</button>
+  </section>
   <section class="card-container">
-    <h1>Grocery Time</h1>
     <section ng-repeat="item in $ctrl.items" class="main">
       <i class="material-icons delete">clear</i>
       <h2>{{item.product}}</h2>
