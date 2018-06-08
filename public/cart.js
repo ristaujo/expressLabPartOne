b0df8048ac972d5c0df8d37e4f271f81a01bbfce
John <ristaujo@gmail.com>
2018-06-07 20:25:44 -0400

    Converted to flex

--- public/cart.js
+++ public/cart.js
@@ -2,11 +2,15 @@
 
 const cart = {
   template: `
-  <h1>Grocery Time</h1>
-  <section ng-repeat="item in $ctrl.items" class="main">
-    <h2>{{item.product}}</h2>
-    <p>Price: $ {{item.price}}</p>
-    <p>Quantity: {{item.quantity}}</p>
+  <section class="card-container">
+    <h1>Grocery Time</h1>
+    <section ng-repeat="item in $ctrl.items" class="main">
+      <i class="material-icons delete">clear</i>
+      <h2>{{item.product}}</h2>
+      <p>Price: $ {{item.price}}</p>
+      <p>Quantity: {{item.quantity}}</p>
+    </section>
+  </section>
   `,
   controller: ["CartService", function(CartService) {
     const vm = this;
